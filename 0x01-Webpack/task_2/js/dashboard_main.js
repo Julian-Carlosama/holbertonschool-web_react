3d728414c3ceb0f4086bf4c42d44bb3d09cd04a7
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-09-15 22:05:33 +0000

    Add task 2

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,20 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$('body').append('<div id="logo"></div>');
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let counter = 0;
+
+// Render and increments counter when the button has been cliked 
+const updateCounter = () => {
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
