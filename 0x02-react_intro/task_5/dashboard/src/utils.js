64e275059be892016036ae83d506f6725827ad7f
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-09-21 23:37:14 -0500

    Add task 8

---
+++ 0x02-react_intro/task_5/dashboard/src/utils.js
@@ -0,0 +1,17 @@
+// Function that return the current year
+export const getFullYear = () => new Date().getFullYear();
+
+//Function that return false or true... 
+// When true, the function should return Holberton School.
+// When false, the function should return Holberton School main dashboard
+export const getFooterCopy = (isIndex) => {
+  if(isIndex) {
+    return "Holberton School";
+  }
+  return "Holberton School main dashboard";
+}
+
+// Function that return a string.
+export const getLatestNotification = () => {
+  return `<strong>Urgent requirement</strong> - complete by EOD`;
+}
