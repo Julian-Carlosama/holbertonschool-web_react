100af6cc7f8b62c79c1dbf409bf8b8f52ee53fcc
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-03 10:19:16 -0500

    Update task 8

--- 0x02-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x02-react_intro/task_5/dashboard/config/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html',
+      template: './public/index.html',
       filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
