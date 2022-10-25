0971f052c349238138d83340fedcb61aa33e6504
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 13:58:55 -0500

    Task 3 done

---
+++ 0x07-React_Immutable/3-list.js
@@ -0,0 +1,13 @@
+import { List } from "immutable";
+
+export function getListObject(array) {
+  const listArr = List(array);
+
+  return listArr;
+}
+
+export function addElementToList(list, element) {
+  const lisPush = list.push(element);
+
+  return lisPush;
+}
