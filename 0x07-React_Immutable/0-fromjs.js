397d1977ae7401bcb2068ffa6c6aa195fad715cd
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 18:17:18 -0500

    Update task 0

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -1,6 +1,12 @@
 import { fromJS } from "immutable";
 
 export default function getImmutableObject(object) {
-    const inmutableObj = fromJS(object)
-    return inmutableObj;
+  const inmutableObj = fromJS(object);
+  return inmutableObj;
 }
+/*console.log(getImmutableObject({
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+}));*/
