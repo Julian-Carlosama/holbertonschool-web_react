772d52a0e4d877c13d6afffc20293cd026c86062
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 12:31:32 -0500

    Update task 1

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -1,13 +1,6 @@
 import { Map } from "immutable";
 
 export default function getImmutableObject(object) {
-    const inmutableMap = Map(object)
-    return inmutableMap;
+    return Map(object);;
 }
 
-console.log(getImmutableObject({
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-}));
