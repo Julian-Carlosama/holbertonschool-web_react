7baec48873270afb9283c54af3a07cdf79446a60
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 19:03:49 -0500

    Task 5 update

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -4,12 +4,12 @@ export function concatElements(page1, page2) {
   const listP1 = List(page1);
   const listP2 = List(page2);
 
-  return listP1.concatE(listP2);
+  return listP1.concat(listP2);
 }
 
 export function mergeElements(page1, page2) {
   const mapP1 = Map(page1);
   const mapP2 = Map(page2);
 
-  return mapP1.mergeE(mapP2);
+  return mapP1.merge(mapP2);
 }
