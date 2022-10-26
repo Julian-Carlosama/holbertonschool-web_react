2ee5e76454534d2b477a8698da51438bb51288e8
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 19:19:05 -0500

    Task 3 fixed

--- 0x07-React_Immutable/3-list.js
+++ 0x07-React_Immutable/3-list.js
@@ -1,12 +1,12 @@
 import { List } from "immutable";
 
-export function getListObject(array) {
+export default function getListObject(array) {
   const listArr = List(array);
 
   return listArr;
 }
 
-export function addElementToList(list, element) {
+export default function addElementToList(list, element) {
   const lisPush = list.push(element);
 
   return lisPush;
