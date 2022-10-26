8f384c6b8434290b5119aecddbe2b0e134416d49
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 20:37:56 -0500

    Fix task 8

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -4,8 +4,8 @@ const { Seq } = immutable;
 
 const makeUppercase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-export default function printBestStudents(object) {
-  const bestStudents = Seq(object)
+export default function printBestStudents(grades) {
+  const bestStudents = Seq(grades)
     .filter((student) => student.score > 70)
     .map((student) => {
       const { firstName, lastName } = student;
