6fa9e5c3735cd3c4735e4c8a0b39a9c58fba1df9
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 19:17:21 -0500

    Task 5 fixed

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,13 +1,13 @@
-import {Map, List} from 'immutable';
+import { Map, List } from 'immutable';
 
-export function concatElements(page1, page2) {
+export default function concatElements(page1, page2) {
   const listP1 = List(page1);
   const listP2 = List(page2);
 
   return listP1.concat(listP2);
 }
 
-export function mergeElements(page1, page2) {
+export default function mergeElements(page1, page2) {
   const mapP1 = Map(page1);
   const mapP2 = Map(page2);
 
