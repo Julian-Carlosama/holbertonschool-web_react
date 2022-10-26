2a728be326a8520d59034a8702e0e4bfd5431dc1
Julian-Carlosama <cromocelcarlosama@gmail.com>
2022-10-25 19:59:02 -0500

    Task 8 updated

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -2,7 +2,7 @@ import { Seq } from "immutable";
 
 const makeUppercase = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-export default function printStudents(grades) {
+export default function printBestStudents(grades) {
   const bestStudents = Seq(grades)
     .filter((student) => student.score > 70)
     .map((student) => {
@@ -16,11 +16,3 @@ export default function printStudents(grades) {
 
   bestStudents.forEach((student) => console.log(student));
 }
-
-/*console.log(printStudents( {
-  1: {
-    score: 99,
-    firstName: 'guillaume',
-    lastName: 'salva',
-  }
-}))/*
